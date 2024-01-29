4d6a056a8b78441af807b4f3aad2348d9f02e144
joaojalssilva <98028549+joaojalssilva@users.noreply.github.com>
2024-01-29 14:41:59 +0000

    last commit of week 1

---
+++ week_1/1_terraform_gcp/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.13.0"
+    }
+  }
+}
+
+provider "google" {
+  # Credentials only needs to be set if you do not have the GOOGLE_APPLICATION_CREDENTIALS set
+  project     = "fine-rookery-412515"
+  region      = "eu-west"
+}
+
+
+
+resource "google_storage_bucket" "demo-bucket" {
+  name     = "fine-rookery-412515-demo-bucket"
+  location = "EU"
+
+  # Optional, but recommended settings:
+  storage_class               = "STANDARD"
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30 // days
+    }
+  }
+
+  force_destroy = true
+}
+
+
